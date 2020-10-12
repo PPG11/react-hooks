e4140816a639b8c711b9feafb81ccfb81e91cf73
yqrashawn <namy.19@gmail.com>
2020-10-12 12:19:10 +0800

    feat: gh-pages

--- index.js
+++
@@ -1,7 +0,0 @@
-export {
-  default as useConfluxPortal,
-  wrapIsPortalInstalled,
-} from "./src/useConfluxPortal";
-export { default as useEpochNumber } from "./src/useEpochNumber";
-export { default as useSWR } from "./src/swr";
-export { default as initContract } from "./src/initContract";
